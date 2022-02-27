b0de2c5b4c3c2fc8e4068fa53681881059fa4887
Faizan <faizanaslam455@gmail.com>
2022-02-27 19:22:09 +0500

    d

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+
+]
